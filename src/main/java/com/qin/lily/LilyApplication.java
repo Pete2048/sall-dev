a419ccd710e3fb180279a69411f3ab9857dbad51
pete <570890581@qq.com>
2020-05-02 11:21:11 +0800

    5555

--- src/main/java/com/qin/lily/LilyApplication.java
+++ src/main/java/com/qin/lily/LilyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LilyApplication {
-    //提交git  sshkey设置
+    //提交git  sshkey设置 555
     public static void main(String[] args) {
         SpringApplication.run(LilyApplication.class, args);
     }
