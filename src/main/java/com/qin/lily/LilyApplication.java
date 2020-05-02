1d30cbe47c0b4caf84e6c13f0afffaebc640f091
pete <570890581@qq.com>
2020-05-02 09:23:26 +0800

    Pete

---
+++ src/main/java/com/qin/lily/LilyApplication.java
@@ -0,0 +1,13 @@
+package com.qin.lily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LilyApplication.class, args);
+    }
+
+}
